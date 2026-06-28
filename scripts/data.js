1641dfb0c1dd3b36be8cc0b2d7e5cbabd2d33498
Roman Pigunov <jerome.malone@yandex.ru>
2026-06-28 11:10:06 +0300

    1

--- scripts/data.js
+++ scripts/data.js
@@ -1,32 +1,36 @@
 export const players = {
   goalkeepers: {
     suzuki: ["Zion", "Suzuki", "Japan", 2, 1],
-    maignan: ["Mike", "Maignan", "France", 6, 1],
+    maignan: ["Mike", "Maignan", "France", 14, 2],
   },
   defenders: {
     guehi: ["Marc", "Guehi", "England", 1, 1],
-    vanDijk: ["Virgil", "Van Dijk", "Netherlands", 7, 1],
-    dumfries: ["Denzel", "Dumfries", "Netherlands", 1, 1],
+    vanDijk: ["Virgil", "Van Dijk", "Netherlands", 13, 2],
+    dumfries: ["Denzel", "Dumfries", "Netherlands", 7, 2],
     nunoMendes: [null, "Nuno Mendes", "Portugal", 16, 2],
-    pacho: ["Willian", "Pacho", "Ecuador", 7, 1],
+    pacho: ["Willian", "Pacho", "Ecuador", 10, 2],
     laryea: ["Richie", "Laryea", "Canada", 8, 1],
   },
   midfielders: {
     doku: ["Jeremy", "Doku", "Belgium", 4, 1],
     onana: ["Amadou", "Onana", "Belgium", 2, 1],
     semenyo: ["Antoine", "Semenyo", "Ghana", 8, 2],
-    caicedo: ["Moises", "Caicedo", "Ecuador", 7, 1],
+    caicedo: ["Moises", "Caicedo", "Ecuador", 12, 2],
     kone: ["Ismael", "Kone", "Canada", 2, 1],
-    diomande: ["Yan", "Diomande", "Côte d'Ivoire", 3, 1],
+    diomande: ["Yan", "Diomande", "Côte d'Ivoire", 10, 2],
+    wissa: ["Yoane", "Wissa", "Congo DR", 14, 1],
+    sarr: ["Ismaila", "Sarr", "Senegal", 12, 1],
+    bellingham: ["Jude", "Bellingham", "England", 13, 1],
   },
   forwards: {
     lukaku: ["Romelu", "Lukaku", "Belgium", 1, 1],
-    mbappe: ["Kylian", "Mbappe", "France", 46, 2],
+    mbappe: ["Kylian", "Mbappe", "France", 62, 3],
     balogun: ["Folarin", "Balogun", "USA", 14, 2],
     david: ["Jonathan", "David", "Canada", 2, 1],
+    jackson: ["Nicolas", "Jackson", "Senegal", 1, 1],
   },
 }; // Игроки
-export const teams = {
+const teams = {
   Japan: "JPN",
   France: "FRA",
   England: "ENG",
@@ -38,4 +42,6 @@ export const teams = {
   Ghana: "GHA",
   "Côte d'Ivoire": "CIV",
   USA: "USA",
+  "Congo DR": "COD",
+  Senegal: "SEN",
 }; // Сборные
