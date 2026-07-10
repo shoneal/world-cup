8bb89b862e3f0fa37db118e089c3dceb3a244d36
Roman Pigunov <jerome.malone@yandex.ru>
2026-07-10 21:20:27 +0300

    1

--- scripts/data.js
+++ scripts/data.js
@@ -1,7 +1,7 @@
 const players = {
   goalkeepers: {
     suzuki: ["Zion", "Suzuki", "Japan", 2, 1],
-    maignan: ["Mike", "Maignan", "France", 27, 4],
+    maignan: ["Mike", "Maignan", "France", 33, 5],
   },
   defenders: {
     guehi: ["Marc", "Guehi", "England", 2, 2],
@@ -13,6 +13,7 @@ const players = {
     tah: ["Jonathan", "Tah", "Germany", 3, 1],
     brown: ["Nathaniel", "Brown", "Germany", 3, 1],
     freeman: ["Alex", "Freeman", "USA", 1, 1],
+    kounde: ["Jules", "Kounde", "France", 6, 1],
   },
   midfielders: {
     doku: ["Jeremy", "Doku", "Belgium", 4, 1],
@@ -25,13 +26,13 @@ const players = {
     sarr: ["Ismaila", "Sarr", "Senegal", 24, 2],
     bellingham: ["Jude", "Bellingham", "England", 30, 3],
     nmecha: ["Felix", "Nmecha", "Germany", 1, 1],
-    dembele: ["Ousmane", "Dembele", "France", 9, 2],
+    dembele: ["Ousmane", "Dembele", "France", 19, 3],
     quinones: ["Julian", "Quinones", "Mexico", 8, 1],
-    lerma: ["Jefferson", "Lerma", "Colombia", 6, 1]
+    lerma: ["Jefferson", "Lerma", "Colombia", 6, 1],
   },
   forwards: {
     lukaku: ["Romelu", "Lukaku", "Belgium", 1, 1],
-    mbappe: ["Kylian", "Mbappe", "France", 96, 5],
+    mbappe: ["Kylian", "Mbappe", "France", 110, 6],
     balogun: ["Folarin", "Balogun", "USA", 22, 4],
     david: ["Jonathan", "David", "Canada", 8, 3],
     jackson: ["Nicolas", "Jackson", "Senegal", 1, 1],
